0e862ca4586d311631041d96b54df7d946e96393
Kc-2004 <91918257+Kc-2004@users.noreply.github.com>
2024-08-16 11:00:51 -0600

    Add files via upload

---
+++ EjerciciosPOE/Ejercicio4/VocalesApp/VocalesApp/Forms/ValidatingLetterForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace VocalesApp.Forms
+{
+    partial class ValidatingLetterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            ValidatingLetterTextBox = new TextBox();
+            validatingErrorProvider = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)validatingErrorProvider).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(249, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(161, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Control de Letras";
+            // 
+            // ValidatingLetterTextBox
+            // 
+            ValidatingLetterTextBox.Location = new Point(249, 68);
+            ValidatingLetterTextBox.Multiline = true;
+            ValidatingLetterTextBox.Name = "ValidatingLetterTextBox";
+            ValidatingLetterTextBox.Size = new Size(161, 35);
+            ValidatingLetterTextBox.TabIndex = 1;
+            ValidatingLetterTextBox.KeyUp += ValidatingLetterTextBox_KeyUp;
+            // 
+            // validatingErrorProvider
+            // 
+            validatingErrorProvider.ContainerControl = this;
+            // 
+            // ValidatingLetterForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ValidatingLetterTextBox);
+            Controls.Add(label1);
+            Name = "ValidatingLetterForm";
+            Text = "ValidatingLetterForm";
+            ((System.ComponentModel.ISupportInitialize)validatingErrorProvider).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox ValidatingLetterTextBox;
+        private ErrorProvider validatingErrorProvider;
+    }
+}
---
+++ EjerciciosPOE/Ejercicio4/VocalesApp/VocalesApp/Forms/ValidatingLetterForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VocalesApp.Forms
+{
+    public partial class ValidatingLetterForm : Form
+    {
+        public ValidatingLetterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ValidatingLetterTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                // Definir las vocales que no deben ser permitidas
+                char[] vocales = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+                // Verificar si la tecla presionada es una vocal
+                if (Array.Exists(vocales, vocal => vocal == (char)e.KeyCode))
+                {
+
+                    validatingErrorProvider.SetError(ValidatingLetterTextBox, "No debe ingresar vocales"); ;
+                    ValidatingLetterTextBox.Focus();
+                    MessageBox.Show("Error", "No puede ingresar vocales",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    // Mostrar el mensaje de advertencia
+                 
+                  
+
+                    // Eliminar el último carácter ingresado (la vocal)
+                    if (ValidatingLetterTextBox.Text.Length > 0)
+                    {
+                        ValidatingLetterTextBox.Text = ValidatingLetterTextBox.Text.Remove(ValidatingLetterTextBox.Text.Length - 1);
+                        ValidatingLetterTextBox.SelectionStart = ValidatingLetterTextBox.Text.Length; // Mover el cursor al final del texto
+                        
+                        
+
+                    }
+                }
+                else
+                {
+                    validatingErrorProvider.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Mostrar mensaje de error en caso de excepción
+                MessageBox.Show($"Se produjo un error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+       
+    }
+}
